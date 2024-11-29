2e728a39c222723fe493cae7c0570f21665d2afe
Rakshana1406 <rrakshana1406@gmail.com>
2024-11-29 09:13:02 +0530

    python lab 4.py

--- python lab 4.py
+++ python lab 4.py
@@ -1,12 +1,9 @@
 num=int(input("Enter the number : "))
 a=0
 b=1
-next_num=b
-count=1
-
-while count <= num:
-    print(next_num,end = "  ")
-    count +=1
-    a,b=b,next_num
-    next_num=a+b
-print()    
+print("The Fibonacci Series :")
+for i in range(num):
+    print(a)
+    c=a+b
+    a=b
+    b=c
